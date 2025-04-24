8e6138c586921530e1679e5086afc701a7eb686a
Hergllen <hergllen@outlook.com>
2025-04-23 20:32:24 -0400

    commit 4

--- aula_002d.py
+++ aula_002d.py
@@ -1,7 +1,2 @@
-dicionario = {'str': "String",
-              'float': 4.5,
-              'int': 10,
-              'bool': True}
-
-for chave, valor in dicionario.items():
-    print(f'{chave}: {valor}')
+def dizeroi()
+    print(dizeroi)
